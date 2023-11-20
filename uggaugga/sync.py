1ae80fe13eac6f6718dd9d9b4584d349ea4419ed
marco <lordscapoladestra@gmail.com>
2023-11-20 13:20:31 +0100

    .

---
+++ uggaugga/sync.py
@@ -0,0 +1,42 @@
+import client
+import json
+import sys
+
+conf = None
+with open('./uggaugga_config.json', 'r') as fp:
+    conf = json.load(fp)
+
+if not conf:
+    raise Exception("file ./uggaugga_config.json not found")
+
+
+DRY_RUN = False
+if '--n' in sys.argv:
+    DRY_RUN = True
+
+extractors = []
+for extr in conf['extractors']:
+    
+    if extr['type'] == 'TExtractor':
+        extractors.append(client.TExtractor(
+            root=extr['root'], 
+            exts=extr['extention_list']))
+        
+    elif extr['type'] == 'XgettexExtractor':
+        extractors.append(client.XgettexExtractor(
+            root=extr['root'],
+            ext=extr['extension'],
+            language=extr['language'], 
+            I18n_parent_key=extr['I18n_parent_key']))
+
+
+client.config(namespace=conf['namespace'],
+              connection_url=conf['server']['url'],
+              public_key=conf['server']['public_key'],
+              secret_key=conf['server']['secret_key'],
+              supported_langs=conf['langs'],
+              i18n_local_path=conf["i18n_path"],
+              code_extractors=extractors)
+
+I18N = client.sync(extract_from_code=True,
+                   dry_run=DRY_RUN or conf.get('debug', False))
