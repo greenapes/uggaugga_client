5f16f5b1cebab5cfbeb404311fc81da87f151fea
marco <lordscapoladestra@gmail.com>
2023-12-11 17:39:23 +0100

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='uggaugga_client',
-    version='0.2.13',    
+    version='0.2.15',    
     description='Client for uggaugga service',
     url='https://github.com/greenapes/uggaugga_client',
     author='Marco Pretelli',
